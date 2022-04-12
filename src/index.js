a0fda25a75ed01b14b6b52a3d30afb43a61abb0b
Johannes.W <johannes@wolfig.net>
2022-04-12 20:10:46 +0200

    reformat index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const core = require('@actions/core');
-const rcon = require('./node-rcon/RCON.js');
+const core = require('@actions/core')
+const rcon = require('./node-rcon/RCON.js')
 
 async function run() {
-	try {
-    const address = core.getInput('server');
-    const port = core.getInput('port');
-    const password = core.getInput('password');
-    const commands = JSON.parse(core.getInput('commands'));
+  try {
+    const address = core.getInput('server')
+    const port = core.getInput('port')
+    const password = core.getInput('password')
+    const commands = JSON.parse(core.getInput('commands'))
 
-		const server = new rcon()
-		await server.connect(address, port, password)
+    const server = new rcon()
+    await server.connect(address, port, password)
 
-		if (commands.length > 0)
-			for (let command of commands) await server.send(command)
+    if (commands.length > 0)
+      for (let command of commands) await server.send(command)
 
-		server.end()
-	} catch (error) {
-		core.setFailed(error.message)
-	}
+    server.end()
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 run()
