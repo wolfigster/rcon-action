748f2c12e9c7c854ffb97352ba56a2dae0148ae0
Johannes.W <johannes@wolfig.net>
2022-04-13 23:14:00 +0200

    fix version and add commands

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ const rcon = require('./node-rcon/RCON.js')
 
 async function run() {
   try {
+    // get input from github actions
     const address = core.getInput('server')
     const port = core.getInput('port')
     const password = core.getInput('password')
     const commands = JSON.parse(core.getInput('commands'))
 
+    // start rcon connection
     const server = new rcon()
     await server.connect(address, port, password)
 
+    // execute commands if there are any
     if (commands.length > 0)
       for (let command of commands) await server.send(command)
 
+    // close connection
     server.end()
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
+// run action
 run()
